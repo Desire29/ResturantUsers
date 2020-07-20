f7ac3d7d1d9ee6095d9f72d766a95ef0de37c470
Cheekezie <47220057+cheekezie@users.noreply.github.com>
2020-07-20 22:58:30 +0100

    cart functionality added

--- food.js
+++ food.js
@@ -36,6 +36,14 @@ const menu = [
         qty:1
     },
 ]
+
+let count = document.getElementById('count');
+function cartCount(){
+    let existingOrder = JSON.parse(localStorage.getItem('order'));
+    if(existingOrder){
+        count.innerHTML = existingOrder.length;
+    }
+}
 function addCart(e){
     let order = menu.filter(item=>{
         return item.id == e
@@ -47,13 +55,65 @@ function addCart(e){
     }
     else{
         localStorage.setItem('order', JSON.stringify(order));
+        count.innerHTML = 1;
     }
+    count.innerHTML = existingOrder.length;
     let alert = document.getElementById('alert')
     let itemAdded = document.getElementById('item');
-    itemAdded.innerHTML = order[0].item;
+    itemAdded.innerHTML = order[0].item + ' ' + 'has been successfully added to cart'
     alert.classList.add('show');
     setTimeout(function(){ alert.classList.remove('show') },5000);
 }
 function viewCart(){
     let existingOrder = JSON.parse(localStorage.getItem('order'));
+    if(existingOrder){
+        let table = document.getElementById('table');
+        table.querySelectorAll('*').forEach(n => n.remove());
+        let sum = 0;
+        existingOrder.forEach(element => {
+            let row = document.createElement('tr')
+            let index = document.createElement('th')
+            index.innerHTML = existingOrder.indexOf(element) + 1;
+            let col1 = document.createElement('td');
+            col1.innerHTML = element.item
+            let col2 = document.createElement('td');
+            col2.innerHTML = element.qty
+            let col3 = document.createElement('td');
+            col3.innerHTML = element.price
+            row.append(index,col1,col2,col3);
+            table.append(row);
+            sum = sum + element.price;
+        });
+        let total = document.createElement('tr');
+        let empty = document.createElement('td');
+        let col = document.createElement('td');
+        col.colSpan = 2;
+        col.innerHTML = 'Total';
+        let price = document.createElement('td');
+        price.innerHTML = 'N' + sum
+        total.append(empty,col,price)
+        table.append(total)
+    }
+}
+function empty(){
+    localStorage.clear();
+    let table = document.getElementById('table');
+    table.querySelectorAll('*').forEach(n => n.remove());
+    count.innerHTML = 0;
+    let alert = document.getElementById('alert')
+    let info = document.getElementById('item');
+    info.innerHTML = 'Cart Successfully empied';
+    alert.classList.add('show');
+    setTimeout(function(){ alert.classList.remove('show') },5000);
+}
+function submit(){
+    localStorage.clear();
+    let table = document.getElementById('table');
+    table.querySelectorAll('*').forEach(n => n.remove());
+    count.innerHTML = 0;
+    let alert = document.getElementById('alert')
+    let info = document.getElementById('item');
+    info.innerHTML = 'Your Order has been placed, You will be notified';
+    alert.classList.add('show');
+    setTimeout(function(){ alert.classList.remove('show') },8000);
 }
